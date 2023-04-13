76e513849349e4a41043da84ad27b514d42ddd85
rohitdhimal <rohit.dhimal19@gmail.com>
2023-04-12 20:23:30 -0400

    frontend rohoit

--- frontend/src/Pages/Documentation/Documentation.jsx
+++ frontend/src/Pages/Documentation/Documentation.jsx
@@ -1,11 +1,18 @@
 import { Fragment } from "react"
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+import './Documentation.css';
 
 const Documentation = () => {
     return (
-        <Fragment>
-            Documentation
-        </Fragment>
-    )
+      <Fragment>
+        <div className="text-center">
+          <p className="">QUIZSHOTZ</p>
+          <p className="">BitsLord</p>
+          <p className="">Team Members</p>
+        </div>
+      </Fragment>
+    );
 }
 
 export default  Documentation
